a3ee55ed69a55c9423abdc93468f3e34415ed75d
Mayank <mayank.manit.k@gmail.com>
2025-07-19 15:57:36 -0500

    Fix build issues.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,16 +3,12 @@ import "./globals.css";
 import type { Metadata } from "next";
 import Footer from "./components/home/Footer";
 import GetInTouch from "./components/home/GetInTouch";
-import AppointmentForm from "./components/home/Appointment-Form
+import AppointmentForm from "./components/home/Appointment-Form";
 // import TestimonialSection from "./components/home/Testimonials";
 import Testimonial2Section from "./components/home/Testimonial2";
 import NavBar from "./components/home/Navbar";
 import { cn } from '@/lib/utils';
 import {
-	raleway,
-	poppins,
-	libreBaskerville,
-	inter,
 	playball,
 } from "@/lib/fonts";
 
