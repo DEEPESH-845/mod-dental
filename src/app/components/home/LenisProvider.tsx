fb2e7ac4d4093abbc2b26bddea38f87cdf2c4865
DEEPESH-845 <dkakkar_be23@thapar.edu>
2025-07-21 23:37:29 +0530

    Optimized Lenis

--- src/app/components/home/LenisProvider.tsx
+++ src/app/components/home/LenisProvider.tsx
@@ -12,12 +12,12 @@ export default function LenisProvider({ children }: LenisProviderProps) {
 		<ReactLenis
 			root
 			options={{
-				lerp: 0.1,
+				lerp: 0.05,
 				duration: 5, // Duration of the scroll animation
 				smoothWheel: true, // Enable smooth scrolling with the mouse wheel
-				syncTouch: false, // Correct touch option
+				syncTouch: true, // Correct touch option
 				syncTouchLerp: 0.075, // Touch inertia lerp
-				touchInertiaExponent: 1.7, // Touch inertia strength
+				touchInertiaExponent: 1, // Touch inertia strength
 				touchMultiplier: 1, // Touch event multiplier
 				wheelMultiplier: 1,
 				gestureOrientation: "vertical",
